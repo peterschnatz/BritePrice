b3d7ce5c3850c3c125e219d2397ddc7374bcf1d6
Peter Schnatz <peter.schnatz@gmail.com>
2019-09-27 03:32:37 -0400

    locally working app

--- chromeExtensionRP/popup.js
+++ chromeExtensionRP/popup.js
@@ -5,8 +5,31 @@ chrome.runtime.sendMessage({"message": "popup_request"});
 chrome.runtime.onMessage.addListener(
 	function(request,sender,sendResponse) {
 		if (request.message === "ready_to_post") {
-			document.getElementById("price").innerHTML = 'New stuff';
+
 			document.getElementById("title").innerHTML = request.eventDetails.title;
+
+			console.log(JSON.stringify(request.eventDetails))
+			xhr = new XMLHttpRequest();
+			xhr.open("POST","http://localhost:5000");
+			//xhr.open("POST","http://18.219.247.228:5000")
+			xhr.send(JSON.stringify(request.eventDetails));
+
+
+			xhr.onreadystatechange = function() {
+    			if (xhr.readyState == 4 && xhr.status == 200) {
+    				 //Request was successful
+    				//let jsonResponse = JSON.parse(xhr.responseText)
+    				console.log(xhr.response)
+    				document.getElementById("price").innerHTML = xhr.response
+			
+
+    			}
+			};
+			//document.getElementById("price").innerHTML = 'New stuff';
+			//document.getElementById("title").innerHTML = request.eventDetails.title;
+
+
+
 		}
 	}
 );
